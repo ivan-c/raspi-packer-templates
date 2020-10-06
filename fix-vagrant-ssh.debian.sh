bf3ca17b76b455888023b8656e53f61573e250b6
Ivan Cvitkovic <icvitkovic@gmail.com>
2020-10-05 17:19:35 -0700

    Install wget; fixup repo URL

--- fix-vagrant-ssh.debian.sh
+++ fix-vagrant-ssh.debian.sh
@@ -3,10 +3,10 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update &&
-    apt-get install -y ca-certificates &&
+    apt-get install -y ca-certificates wget &&
     mkdir -p /home/vagrant/.ssh &&
-    wget -O /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub &&
+    wget -O /home/vagrant/.ssh/authorized_keys https://raw.github.com/hashicorp/vagrant/master/keys/vagrant.pub &&
     chown -R vagrant:vagrant /home/vagrant/.ssh &&
     chmod 0700 /home/vagrant/.ssh &&
     chmod 0600 /home/vagrant/.ssh/authorized_keys &&
-    apt-get purge -y ca-certificates
+    apt-get purge -y ca-certificates wget
