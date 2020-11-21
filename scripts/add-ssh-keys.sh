8c7fa60a4f0328d925557d94acbe5f137b640f17
Ivan Cvitkovic <icvitkovic@gmail.com>
2020-11-21 11:07:34 -0800

    Do not install vagrant ssh keys

--- scripts/add-ssh-keys.sh
+++ scripts/add-ssh-keys.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -e
 
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y ca-certificates wget
+
 
 test -d ~root/.ssh || mkdir ~root/.ssh
 wget -O - https://github.com/ivan-c.keys > ~root/.ssh/authorized_keys
