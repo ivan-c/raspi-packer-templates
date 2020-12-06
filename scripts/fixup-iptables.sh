f375b5ad14e1d810c39a1abe481037a463c52a8f
Ivan Cvitkovic <icvitkovic@gmail.com>
2020-12-05 16:42:21 -0800

    Add nftables workaround Add script to enable iptables-legacy until k8s supports nftables

---
+++ scripts/fixup-iptables.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -eu
+# use nftables legacy iptables frontend until kubernetes supports nftables natively
+
+
+# TODO remove script when native k8s nftables support available
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y iptables arptables ebtables
+
+# switch to legacy versions
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+update-alternatives --set arptables /usr/sbin/arptables-legacy
+update-alternatives --set ebtables /usr/sbin/ebtables-legacy
